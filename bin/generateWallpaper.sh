d60dd562ac5a6bc65b5a98470d5fced9a599bd3d
Rene <rene@sdo.sh>
2020-10-24 20:42:15 +0200

    feature: made generator script self-contained, added 1280x1024 wallpaper

--- bin/generateWallpaper.sh
+++ bin/generateWallpaper.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-WALLPAPER_SVG=~/path/to/debian-greyish-wallpaper-widescreen.svg
+WALLPAPER_SVG=./img/templates/debian-greyish-wallpaper-widescreen.svg
 WALLPAPER_PNG=debian-greyish-wallpaper-widescreen.png
 LOGO_SVG=logo.svg
 LOGO_PNG=logo.png
 
 WORK_DIR=/tmp/
 
-SIZES=("1440x900" "1440x1050" "1920x1080" "1600x1200" "1920x1200" "2560x1440" "3200x1800" "2560x2048" "1366x768" "4080x768")
+SIZES=("1280x1024", "1440x900" "1440x1050" "1920x1080" "1600x1200" "1920x1200" "2560x1440" "3200x1800" "2560x2048" "1366x768" "4080x768")
 
-wget https://example.com/logo_schmidt_devops.svg -O ${WORK_DIR}${LOGO_SVG}
+cp ./img/logo/logo_schmidt_devops.svg ${WORK_DIR}${LOGO_SVG}
 
-convert -background none -resize 50% ${WORK_DIR}${LOGO_SVG} png32:${WORK_DIR}${LOGO_PNG}
+convert -transparent "#ffffff" -resize 50% ${WORK_DIR}${LOGO_SVG} png32:${WORK_DIR}${LOGO_PNG}
 
 for size in "${SIZES[@]}"
 do
